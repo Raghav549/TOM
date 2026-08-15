33ae0d0116fe7b3aa987f1025aa6d0e85dbd7b7b
zyra noxe <raghavkashyap2229@gmail.com>
2026-08-15 13:00:34 +0530

    Add Android companion app module configuration

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.tom.device"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.tom.device"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+
+    buildFeatures {
+        buildConfig = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.activity:activity-ktx:1.10.1")
+}
