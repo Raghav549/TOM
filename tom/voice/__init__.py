04bf105595b4015cba2afbd38c597c6db6242a6f
zyra noxe <raghavkashyap2229@gmail.com>
2026-08-15 16:59:35 +0530

    feat(voice): export Indic Parler and Smart Turn adapters

--- tom/voice/__init__.py
+++ tom/voice/__init__.py
@@ -4,37 +4,22 @@ from .asr import FasterWhisperASR
 from .cosyvoice_stream import CosyVoiceStreamingAdapter, TTSChunk
 from .director import VoiceDirector
 from .engine import SpeechEngine, SpeechEngineConfig
+from .indic_parler_stream import IndicParlerStreamingAdapter
 from .models import Emotion, Language, VoiceProfile, VoiceStyle
 from .neural_vad import SileroStreamingVAD, VADDecision
 from .prosody import ExpressiveSpeechPlanner, PCM16ProsodyExtractor
 from .prosody_state import ContinuousProsodyState, ContinuousProsodyTracker
+from .smart_turn_onnx import SmartTurnDecision, SmartTurnONNX
 from .streaming_asr import PartialTranscript, StreamingFasterWhisper
 from .turn_predictor import LearnedTurnPredictor, TurnPrediction
 from .turntaking import DuplexState, DuplexTurnManager, TurnDecision, TurnSignal
 
 __all__ = [
-    "ContinuousProsodyState",
-    "ContinuousProsodyTracker",
-    "CosyVoiceStreamingAdapter",
-    "DuplexState",
-    "DuplexTurnManager",
-    "Emotion",
-    "ExpressiveSpeechPlanner",
-    "FasterWhisperASR",
-    "Language",
-    "LearnedTurnPredictor",
-    "PCM16ProsodyExtractor",
-    "PartialTranscript",
-    "SileroStreamingVAD",
-    "SpeechEngine",
-    "SpeechEngineConfig",
-    "StreamingFasterWhisper",
-    "TTSChunk",
-    "TurnDecision",
-    "TurnPrediction",
-    "TurnSignal",
-    "VADDecision",
-    "VoiceDirector",
-    "VoiceProfile",
-    "VoiceStyle",
+    "ContinuousProsodyState", "ContinuousProsodyTracker", "CosyVoiceStreamingAdapter",
+    "DuplexState", "DuplexTurnManager", "Emotion", "ExpressiveSpeechPlanner",
+    "FasterWhisperASR", "IndicParlerStreamingAdapter", "Language", "LearnedTurnPredictor",
+    "PCM16ProsodyExtractor", "PartialTranscript", "SileroStreamingVAD", "SmartTurnDecision",
+    "SmartTurnONNX", "SpeechEngine", "SpeechEngineConfig", "StreamingFasterWhisper",
+    "TTSChunk", "TurnDecision", "TurnPrediction", "TurnSignal", "VADDecision",
+    "VoiceDirector", "VoiceProfile", "VoiceStyle",
 ]
